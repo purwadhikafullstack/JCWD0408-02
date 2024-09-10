8b68acccd47a9a9be8902106749adf74c7afb15c
VinsenJovin <vinjovinda@gmail.com>
2024-09-10 11:23:30 +0700

    feat: delete flowbite

--- apps/web/src/app/(home)/reservation/_components/paymentMethod.tsx
+++ apps/web/src/app/(home)/reservation/_components/paymentMethod.tsx
@@ -1,42 +1,41 @@
-'use client';
-import { Dropdown } from 'flowbite-react';
-import DropdownPay from './dropdownPay';
-import { useState } from 'react';
+"use client";
+import DropdownPay from "./dropdownPay";
+import { useState } from "react";
 export default function PaymentMethod() {
-  const [drop, setDrop] = useState<Boolean>(false);
-  const [payMethod, setPayMethod] = useState<String>('Virtual Account');
+  const [drop, setDrop] = useState<boolean>(false);
+  const [payMethod, setPayMethod] = useState<string>("Virtual Account");
 
   const handleDropdownVA = () => {
-    setPayMethod('Virtual Account');
+    setPayMethod("Virtual Account");
     setDrop(false);
   };
   const handleDropdownTF = () => {
-    setPayMethod('Transfer Bank');
+    setPayMethod("Transfer Bank");
     setDrop(false);
   };
   const handleDrop = () => {
     setDrop(!drop);
   };
   return (
-    <div className="py-4 ">
+    <div className="py-4">
       <div className="py-4">
-        <h1 className="text-xl font-semibold pb-2">Kebijakan pembatalan</h1>
-        <p>
+        <h1 className="py-1 font-semibold text-xl">Kebijakan pembatalan</h1>
+        <p className="text-lg">
           Pembatalan gratis sebelum 19 Sep. Jika dibatalkan sebelum check-in
           pada tanggal 20 Sep, Anda akan mendapatkan pengembalian uang sebagian.
         </p>
       </div>
       <hr />
-      <div className="py-4 flex flex-col ">
-        <h1 className="text-xl font-semibold pb-2">Aturan dasar</h1>
-        <p>
+      <div className="flex flex-col py-4">
+        <h1 className="pb-2 text-xl font-semibold">Aturan dasar</h1>
+        <p className="text-lg">
           Kami meminta setiap tamu untuk mengingat beberapa hal sederhana
           mengenai apa saja yang perlu dilakukan untuk menjadi tamu yang luar
           biasa.
         </p>
-        <div className="pl-4 py-2">
-          <ul className="list-disc">
-            <li>Patuhi peraturan rumah</li>
+        <div className="py-2 pl-4 text-lg">
+          <ul className="list-disc ">
+            <li className="mb-2">Patuhi peraturan rumah</li>
             <li>
               Perlakukan rumah milik Tuan Rumah Anda seperti rumah Anda sendiri
             </li>
--- apps/web/src/app/(home)/reservation/_components/reservationDetail.tsx
+++ apps/web/src/app/(home)/reservation/_components/reservationDetail.tsx
@@ -1,23 +1,32 @@
-import PaymentMethod from './paymentMethod';
+"use client";
+import DateComp from "@/components/DateComp";
+import PaymentMethod from "./paymentMethod";
+import { useState } from "react";
 
 export default function ReservationDetail() {
+  const [dateState, setDateState] = useState<boolean>(false);
+
   return (
     <div className="lg:w-[35%]">
-      <div className="flex gap-2 pb-5 items-center">
-        <h1 className="text-3xl font-bold ">RESERVASI</h1>
+      <div className="flex items-center gap-2 pb-5">
+        <h1 className="text-3xl font-bold">RESERVASI</h1>
       </div>
-      <h2 className="text-xl text-hitam font-bold">Pemesanan Anda</h2>
-      <div className="grid grid-cols-2 pt-2 pb-4 gap-x-6 gap-y-4 justify-between">
+      <h2 className="text-2xl font-bold text-hitam">Pemesanan Anda</h2>
+      <div className="grid grid-cols-2 justify-between gap-x-6 gap-y-4 pb-4 pt-2">
         <div>
-          <p className="font-semibold py-1">Tanggal</p>
-          <p>26–27 Sep</p>
+          <p className="py-1 text-xl font-semibold">Tanggal</p>
+          <p className="text-lg">26–27 Sep</p>
         </div>
-        <button className="underline underline-offset-4 font-medium text-end">
+        <button
+          onClick={() => setDateState(!dateState)}
+          className="text-end font-medium underline underline-offset-4"
+        >
           Edit
         </button>
+
         <div>
-          <p className="font-semibold py-1">Tamu</p>
-          <p>1 Tamu</p>
+          <p className="py-1 text-xl font-semibold">Tamu</p>
+          <p className="text-lg">1 Tamu</p>
         </div>
       </div>
       <hr />
