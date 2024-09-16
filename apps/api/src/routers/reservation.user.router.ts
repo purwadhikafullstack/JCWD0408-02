1ba1c9234c926bb666d7f9123a4af371f8b27ef7
Dietyy <radietdesfandri@gmail.com>
2024-09-16 13:56:25 +0700

    Develop (#27)
    * Merge branch 'develop' of https://github.com/purwadhikafullstack/JCWD0408-02 into jovin
    
    * feat: menyelesaikan uploader
    
    * feat: design profile user (#25)
    
    * feat: install prettierrc
    
    * feat: nodemailer, and fullstack register and add toast
    
    * feat: add redux
    
    * Develop (#22)
    
    * feat: delete flowbite
    
    * feat: build midtrans transaction
    
    * feat: add redux (#20)
    
    * feat: install prettierrc
    
    * feat: nodemailer, and fullstack register and add toast
    
    * feat: add redux
    
    ---------
    
    Co-authored-by: Vinsensius Jovinda <127381316+vinsenjo@users.noreply.github.com>
    Co-authored-by: VinsenJovin <vinjovinda@gmail.com>
    
    * feat: reset password done
    
    * feat: design profile user
    
    ---------
    
    Co-authored-by: Vinsensius Jovinda <127381316+vinsenjo@users.noreply.github.com>
    Co-authored-by: VinsenJovin <vinjovinda@gmail.com>
    
    * feat: menyelesaikan be uploader
    
    ---------
    
    Co-authored-by: VinsenJovin <vinjovinda@gmail.com>
    Co-authored-by: Vinsensius Jovinda <127381316+vinsenjo@users.noreply.github.com>

--- apps/api/src/routers/reservation.user.router.ts
+++ apps/api/src/routers/reservation.user.router.ts
@@ -1,4 +1,5 @@
 import { ReservationController } from '@/controllers/reservation.user.controller';
+import { uploader } from '@/services/uploader';
 import { Router } from 'express';
 
 export class ReservationRouter {
@@ -17,6 +18,8 @@ export class ReservationRouter {
       '/VA/status',
       this.reservationController.updateStatusTrans,
     );
+    this.router.post('/TF', this.reservationController.createReservationTF);
+    this.router.post('/TF/proof',uploader('proof','/proof').single('media'), this.reservationController.uploadPaymentProof);
   }
   getRouter(): Router {
     return this.router;
